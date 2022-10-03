73c0352720aceb7c6002816fcd7ae313a8e01012
Kristex95 <kristex95@gmail.com>
2022-10-03 17:59:48 +0300

    OOP Lab1 Complete!

--- OOP/Lab1/test/sorting_system_test/SortingSystemTest.java
+++ OOP/Lab1/test/sorting_system_test/SortingSystemTest.java
@@ -1,11 +1,19 @@
 package sorting_system_test;
 
+import container.Package;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
-import sweets.Sweet;
-import container.Package;
+import org.w3c.dom.ls.LSOutput;
 import sorting_system.SortingSystem;
+import sweets.Sweet;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,40 +21,39 @@ public class SortingSystemTest {
      SortingSystem system = new SortingSystem();
 
     @Test
-    public void getTotalMass() {
+    public void getTotalMass() throws IOException {
         Package pkg = new Package();
-        pkg.AddSweets(new Sweet("Lollipop", 50, 200, 10));
-        pkg.AddSweets(new Sweet("Chocolate", 200, 500, 30));
-        pkg.AddSweets(new Sweet("Candy", 65, 110, 8));
-        pkg.AddSweets(new Sweet("Marshmallow", 300, 425, 60));
-        Assert.assertEquals(pkg.GetTotalMass(), 615.0, 0);
-
-        List<Sweet> sweets = new ArrayList<>();
-        sweets.add(new Sweet("Lollipop", 50, 200, 10));
-        sweets.add(new Sweet("Chocolate", 200, 500, 30));
-        sweets.add(new Sweet("Candy", 65, 110, 8));
-        sweets.add(new Sweet("Marshmallow", 300, 425, 60));
-        Assert.assertEquals(system.getTotalMass(sweets), 615.0, 0);
+        try(FileInputStream fileInputStream = new FileInputStream("test/input.json")) {
+            JSONArray object = new JSONArray( new String(fileInputStream.readAllBytes()));
+            for (int i = 0; i < object.length(); i++){
+                pkg.AddSweets(new Sweet(object.getJSONObject(i).getString("Name"), object.getJSONObject(i).getDouble("Mass"), object.getJSONObject(i).getDouble("Energy Value"), object.getJSONObject(i).getDouble("Sugar")));
+            }
+            Assert.assertEquals(pkg.GetTotalMass(), 615.0, 0);
+        }
     }
 
     @Test
-    public void sortByMass() {
-        List<Sweet> sweets = new ArrayList<>();
-        sweets.add(new Sweet("Chocolate", 200, 500, 30));
-        sweets.add(new Sweet("Lollipop", 50, 200, 10));
-        sweets.add(new Sweet("Candy", 65, 110, 8));
-        sweets.add(new Sweet("Marshmallow", 300, 425, 60));
-        sweets = system.sortByMass(sweets);
-        Assert.assertEquals(sweets.get(0).getWeight(), 50.0, 0);
+    public void sortByMass() throws IOException {
+        Package pkg = new Package();
+        try(FileInputStream fileInputStream = new FileInputStream("test/input.json")) {
+            JSONArray object = new JSONArray( new String(fileInputStream.readAllBytes()));
+            for (int i = 0; i < object.length(); i++) {
+                pkg.AddSweets(new Sweet(object.getJSONObject(i).getString("Name"), object.getJSONObject(i).getDouble("Mass"), object.getJSONObject(i).getDouble("Energy Value"), object.getJSONObject(i).getDouble("Sugar")));
+            }
+            pkg.SortByMass();
+            Assert.assertEquals(pkg.GetByID(0).getWeight(), 50.0, 0);
+        }
     }
 
     @Test
-    public void findBySugarRange() {
-        List<Sweet> sweets = new ArrayList<>();
-        sweets.add(new Sweet("Chocolate", 200, 500, 30));
-        sweets.add(new Sweet("Lollipop", 50, 200, 10));
-        sweets.add(new Sweet("Candy", 65, 110, 8));
-        sweets.add(new Sweet("Marshmallow", 300, 425, 60));
-        Assert.assertEquals(system.findBySugarRange(sweets, 0.0, 9.0).get(0).getSugar(), 8.0, 0);
+    public void findBySugarRange() throws IOException {
+        Package pkg = new Package();
+        try(FileInputStream fileInputStream = new FileInputStream("test/input.json")) {
+            JSONArray object = new JSONArray( new String(fileInputStream.readAllBytes()));
+            for (int i = 0; i < object.length(); i++) {
+                pkg.AddSweets(new Sweet(object.getJSONObject(i).getString("Name"), object.getJSONObject(i).getDouble("Mass"), object.getJSONObject(i).getDouble("Energy Value"), object.getJSONObject(i).getDouble("Sugar")));
+            }
+            Assert.assertEquals(system.findBySugarRange(pkg.getSweetsList(), 0.0, 9.0).get(0).getSugar(), 8.0, 0);
+        }
     }
 }
